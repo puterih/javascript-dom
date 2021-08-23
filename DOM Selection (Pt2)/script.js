ea4e4846adf1450c2ea9ce9d3801048ee09c46b1
puteri husnul k <husnulputeri@gmail.com>
2021-08-23 22:11:58 +0700

    latihan D0M Selection(Pt2)

---
+++ DOM Selection (Pt2)/script.js
@@ -0,0 +1,22 @@
+// DOM Selection
+// document.getElementById()
+// document.getElementsByTagName()
+// document.getElementsByClassName()
+
+// document.querySelector()
+// atau disebut menanyakan selector css
+const p4 = document.querySelector('#b p');
+p4.style.color = 'green';
+p4.style.fontSize = '30px';
+
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+li2.style.backgroundColor = 'orange';
+
+
+// document.querySelectorAll()
+const p = document.querySelectorAll('p');
+for( let i = 0; i < p.length; i++ ) {
+	p[i].style.backgroundColor = 'lightblue';
+}
+
+// Day-5
