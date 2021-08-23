1eaa8507e93e36d9454fc6c50b3999e5d3873a66
puteri husnul k <husnulputeri@gmail.com>
2021-08-23 22:24:22 +0700

    Latihan tambahan DOM selection(pt2)

--- DOM Selection (Pt2)/script.js
+++ DOM Selection (Pt2)/script.js
@@ -3,20 +3,27 @@
 // document.getElementsByTagName()
 // document.getElementsByClassName()
 
-// document.querySelector()
+// document.querySelector() - element
 // atau disebut menanyakan selector css
-const p4 = document.querySelector('#b p');
-p4.style.color = 'green';
-p4.style.fontSize = '30px';
+// ---
+// const p4 = document.querySelector('#b p');
+// p4.style.color = 'green';
+// p4.style.fontSize = '30px';
 
-const li2 = document.querySelector('section#b ul li:nth-child(2)');
-li2.style.backgroundColor = 'orange';
+// const li2 = document.querySelector('section#b ul li:nth-child(2)');
+// li2.style.backgroundColor = 'orange';
 
 
-// document.querySelectorAll()
-const p = document.querySelectorAll('p');
-for( let i = 0; i < p.length; i++ ) {
-	p[i].style.backgroundColor = 'lightblue';
-}
+// // document.querySelectorAll()
+// const p = document.querySelectorAll('p');
+// for( let i = 0; i < p.length; i++ ) {
+// 	p[i].style.backgroundColor = 'lightblue';
+// }
+// ---
+
 
 // Day-5
+// node root
+const sectionB = document.querySelector('section#b');
+const p4 = sectionB.getElementsByTagName('p')[0];
+p4.style.backgroundColor = 'orange';
