12a03e7ad18ca40c23957b2c09f5b255a2036f54
puteri husnul k <husnulputeri@gmail.com>
2021-08-24 23:58:06 +0700

    latihan DOM Manipulation(Pt1)

---
+++ DOM Manipulation (Pt1)/script.js
@@ -0,0 +1,18 @@
+// element.innerHTML untuk mengubah isi dari sebuah teks yang sudah diseleksi
+// const judul = document.getElementById('judul');
+// judul.innerHTML = '<em>Puteri husnul</em>';
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = 'hello world!';
+
+
+// const judul = document.querySelector('#judul');
+// judul.style.color = 'lightblue';
+// judul.style.backgroundColor = 'salmon';
+
+
+// atribut contoh: id, class, href
+
+const judul = document.getElementsByTagName
+
+// belum selesai
