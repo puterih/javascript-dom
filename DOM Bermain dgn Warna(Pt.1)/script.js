2a51e905e2b4a273f2fad8c51e3fec79748637fe
puteri husnul k <husnulputeri@gmail.com>
2021-08-28 23:57:56 +0700

    Ltihan.2 DOM bermain dgn warna(Pt.2)

--- DOM Bermain dgn Warna(Pt.1)/script.js
+++ DOM Bermain dgn Warna(Pt.1)/script.js
@@ -13,9 +13,56 @@ tAcakWarna.setAttribute('type', 'button');
 tUbahWarna.after(tAcakWarna);
 
 tAcakWarna.addEventListener('click', function() {
-	document.body.style.backgroundColor = rgb('100,100,100');
+	const r = Math.round(Math.random() * 255 + 1);
+	const g = Math.round(Math.random() * 255 + 1);
+	const b = Math.round(Math.random() * 255 + 1);
+	document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')'
+});
 
-	// BELOM SELESAI... BANYAK TUGAS SEKOLAH :(
-// Lanjut besokk
-// Sinyal lag, jd tdk bisa lanjut liat di yt lptp :(
+
+
+const sMerah = document.querySelector('input[name=sMerah');
+const sHijau = document.querySelector('input[name=sHijau');
+const sBiru = document.querySelector('input[name=sBiru');
+
+sMerah.addEventListener('input', function() {
+	const r = sMerah.value;
+	const g = sHijau.value;
+	const b = sBiru.value;
+	document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+});
+
+sHijau.addEventListener('input', function() {
+	const r = sMerah.value;
+	const g = sHijau.value;
+	const b = sBiru.value;
+	document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+});
+
+sBiru.addEventListener('input', function() {
+	const r = sMerah.value;
+	const g = sHijau.value;
+	const b = sBiru.value;
+	document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+});
+
+
+// document.body.addEventListener('mousemove', function(event) {
+// 	// posisi mouse
+// 	// console.log(event.clientY);
+// 	// ukuran browser
+// 	// console.log(window.innerWidth);
+// 	const xPos = Math.round(event.clientX)
+
+// yg di catatan blm ngerti:(
+// lanjut besok
+
+
+
+
+
+
+
+
+
 
