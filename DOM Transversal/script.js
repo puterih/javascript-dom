e069899e25ae74adef13d3c8da47cced39a7dde4
puteri husnul k <husnulputeri@gmail.com>
2021-09-01 21:14:06 +0700

    Latihan DOM Transversal

---
+++ DOM Transversal/script.js
@@ -0,0 +1,35 @@
+// Event Handling
+// const close = document.querySelector('.close');
+// const card = document.querySelector('.card');
+
+
+// close.addEventListener('click', function() {
+// 	card.style.display = 'none';
+// });
+// ----
+
+// DOM Transversal
+const close = document.querySelectorAll('.close');
+
+// for( let i = 0; i < close.length; i++ ) {
+// 	close[i].addEventListener('click', function(e) {
+// 		// close[i].parentElement.style.display = 'none';
+// 		e.target.parentElement.style.display = 'none';
+// 	});
+// }
+// -- cara lain dibawah ini
+
+close.forEach(function(el) {
+	el.addEventListener('click', function(e) {
+		e.target.parentElement.style.display = 'none';
+	});
+});
+
+const nama = document.querySelector('.nama');
+// bisa cek console
+console.log(nama.parentNode);
+// console.log(nama.parentElement);
+// console.log(nama.nextSibling);
+// console.log(nama.nextElementSibling);
+// console.log(nama.previousSibling);
+// console.log(nama.previousElementSibling);
