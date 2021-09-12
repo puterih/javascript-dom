e84095b8e3246148d470ad30c5c075d21cb853fd
puteri husnul k <husnulputeri@gmail.com>
2021-09-12 19:50:01 +0700

    Latihan Js Lanjutan Prototype Object.create

--- 1.1 OBJECT/script.js
+++ 1.1 OBJECT/script.js
@@ -1,5 +1,6 @@
 // Cara untuk menbuat Object pada Javascript
 // 1. Object literal
+// PROBLEM = Tdak efektif untuk objek jang banyak
 // let mahasiswa1 = {
 // 	nama: 'Puteri',
 // 	energ1: 10,
@@ -19,46 +20,56 @@
 // }
 
 // 2. Function Declaration
-// function Mahasiswa(nama, energi) {
-// 	let mahasiswa = {};
-// 	mahasiswa.nama = nama;
-// 	mahasiswa.energi = energi;
 
-// 	mahasiswa.makan = function (porsi) {
+const methodMahasiswa = {
+	makan: function (porsi) {
+		this.energi += porsi;
+		console.log(`Halo ${this.nama}, selamat makan!`);
+	},
+
+	main: function (jam) {
+		this.energi -= porsi;
+		console.log(`Halo ${this.nama}, selamat Bermain!`);
+	},
+
+	tidur: function(jam) {
+		this.energi += jam * 2;
+		console.log(`Halo ${this.nama}, selamat Tidur!`);
+	}
+};
+
+function Mahasiswa(nama, energi) {
+	let mahasiswa = Object.create(methodMahasiswa);
+// Object.create langsung menunjukkan parent objectnya
+	mahasiswa.nama = nama;
+	mahasiswa.energi = energi;
+
+ return mahasiswa;
+}
+
+let puteri = Mahasiswa('Puteri', 10);
+let husnul = Mahasiswa('husnul', 20);
+
+// 3. Constructor function
+// Keyward new
+// function Mahasiswa(nama, energi) {
+// 	// let mahasiswa = {};  ini dihapus karna otomatis
+// 	this.nama = nama;
+// 	this.energi = energi;
+
+// 	this.makan = function (porsi) {
 // 		this.energi += porsi;
 // 		console.log(`Halo ${this.nama}, selamat makan!`);
 // 	}
 
-// 	mahasiswa.main = function(jam) {
+// 	this.main = function(jam) {
 // 		this.energi -= jam;
 // 		console.log(`Halo ${this.nama}, selamat bermain!`)
 // 	}
-// 	return mahasiswa;
+// 	// return mahasiswa;   dihapus
 // }
 
-// let puteri = Mahasiswa('Puteri', 10);
-// let husnul = Mahasiswa('husnul', 20);
-
-// 3. Constructor function
-// Keyward new
-function Mahasiswa(nama, energi) {
-	// let mahasiswa = {};  ini dihapus karna otomatis
-	this.nama = nama;
-	this.energi = energi;
-
-	this.makan = function (porsi) {
-		this.energi += porsi;
-		console.log(`Halo ${this.nama}, selamat makan!`);
-	}
-
-	this.main = function(jam) {
-		this.energi -= jam;
-		console.log(`Halo ${this.nama}, selamat bermain!`)
-	}
-	// return mahasiswa;   dihapus
-}
-
-let puteri = new Mahasiswa('Puteri', 10);
+// let puteri = new Mahasiswa('Puteri', 10);
 
 
 // 4. 0bject.create
