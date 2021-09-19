5efd498c756b281806a59f14dbd4bbadbd194f6c
puteri husnul k <husnulputeri@gmail.com>
2021-09-19 15:23:52 +0700

    Latihan Var, Let & Const

--- QuickTip #5 Var, Let, Const/script.js
+++ QuickTip #5 Var, Let, Const/script.js
@@ -3,7 +3,21 @@
 // console.log(i);
 // i = 10;
 // ---
-for( var i = 0; i < 10; i++ ) {
-    console.log(i);
-}
+// function tes() {
+//     for( var i = 0; i < 10; i++ ) {
+//         console.log(i);
+//     }
+// }
+// tes();
+// ----
+// for( let i = 0 ; i < 10; i++ ) {
+//         console.log(i);
+//     }
+
+// console.log(i);
+// ----
+const i = [1,2,3]
+
+i.push(4);
+
 console.log(i);
